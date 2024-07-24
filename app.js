7a36bbace78ed95e7afcf3a5289d5fa9c883e204
M-Aloysie <a.murekatet@alustudent.com>
2024-07-24 07:59:40 -0400

    User Registration and Login

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-import express from "express";
-import router from "./routes/router.js";
+import express from 'express';
+import authRoutes from './routes/authRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-// app.use('/fileManager', router);
 
-export default app
+app.use('/api/auth', authRoutes);
+
+export default app;
---
+++ generateKey.mjs
@@ -0,0 +1,1 @@
+import crypto from "crypto"; const secret = crypto.randomBytes(32).toString("hex"); console.log(secret);
