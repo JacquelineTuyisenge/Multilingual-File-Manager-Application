f27c3acf900d88d08fb4780f7f8fd46b21e6e1ad
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-07-26 14:29:59 +0200

    documentation

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import router from './routes/router.js';
 import swaggerDocs from './swaggerConfig.js';
-import swaggerUiExpress from 'swagger-ui-express';             
+import swaggerUiExpress from 'swagger-ui-express';   
+          
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerDocs.serve, swaggerUiExpress.setup(swaggerDocs));
+app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocs));
 app.use('/api/', router);
 
 export default app;
